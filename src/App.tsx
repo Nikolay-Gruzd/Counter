93114ec6f23da5d5fa962cab0caddc3eadafa31a
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-08-11 00:15:57 +0800

    Advanced counter
    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,30 @@ import s from './App.module.css'
 import {useState} from "react";
 import {Counter} from "./components/Counter/Counter.tsx";
 import {ButtonsContainer} from "./components/ButtonsContainer/ButtonsContainer.tsx";
+import {Setting} from "./components/Setting/Setting.tsx";
 
-export const MAX_VALUE = 12
-export const MIN_VALUE = 3
+export const MAX_VALUE = 5
+export const MIN_VALUE = 1
+
+export type SettingType = {
+    max: number,
+    start: number
+}
 
 function App() {
-    const [counter, setCounter] = useState<number>(MIN_VALUE)
+    const [setting, setSetting] = useState<SettingType>({max: MAX_VALUE, start: MIN_VALUE})
+    const [counter, setCounter] = useState<number>(setting.start)
+    const [error, setError] = useState<string>('')
+
     return (
         <div className={s.app}>
             <div className={s.container}>
-
+                <Setting setting={setting} setSetting={setSetting} setCounter={setCounter} setError={setError} />
+                <ButtonsContainer modeSetting={true} setting={setting}/>
             </div>
             <div className={s.container}>
-                <Counter counter={counter}/>
-                <ButtonsContainer counter={counter} setCounter={setCounter}/>
+                <Counter counter={counter} setting={setting} error={error}/>
+                <ButtonsContainer counter={counter} setCounter={setCounter} setting={setting}/>
             </div>
         </div>
     )
