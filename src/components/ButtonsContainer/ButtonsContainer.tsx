35936985894e67e7eb04ac69185b6262fa0c3024
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-08-11 00:16:14 +0800

    Advanced counter
    ButtonsContainer.tsx

--- src/components/ButtonsContainer/ButtonsContainer.tsx
+++ src/components/ButtonsContainer/ButtonsContainer.tsx
@@ -1,25 +1,50 @@
 import s from './ButtonsContainer.module.css'
 import {Button} from "../Button/Button.tsx";
-import {MAX_VALUE, MIN_VALUE} from "../../App.tsx";
+import {MAX_VALUE, MIN_VALUE, SettingType} from "../../App.tsx";
 
 type ButtonsContainerType = {
-    counter: number
-    setCounter: (value: number) => void
+    counter?: number
+    setCounter?: (value: number) => void
+    modeSetting?: boolean
+    setting: SettingType
 }
 
-export const ButtonsContainer = ({counter, setCounter}: ButtonsContainerType) => {
+export const ButtonsContainer = ({counter, setCounter, modeSetting, setting}: ButtonsContainerType) => {
+    if (modeSetting) {
+        const disabledButton =
+            setting.start === MIN_VALUE
+            && setting.max === MAX_VALUE
+            || setting.start < 0
+            || setting.max < 0
+            || setting.start > setting.max
+            || setting.start === setting.max
+        return (
+            <div className={s.buttonsContainer}>
+                <Button name='set' disabled={disabledButton} onClick={() => {}}/>
+            </div>
+        )
+    }
 
     const onClickButtonInc = () => {
-        let inc = counter + 1
-        setCounter(inc)
+        counter !== undefined && setCounter?.(counter + 1)
     }
     const onClickButtonReset = () => {
-        setCounter(MIN_VALUE)
+        setCounter?.(setting.start)
     }
+
+    const incDisabledValue = counter === undefined
+        || counter === setting.max || setting.start !== MIN_VALUE || setting.max !== MAX_VALUE
+
+    const resetDisabledValue = counter === undefined
+        || counter === setting.start || setting.start !== MIN_VALUE || setting.max !== MAX_VALUE
+
     return (
         <div className={s.buttonsContainer}>
-            <Button name='inc' disabled={counter >= MAX_VALUE} onClick={onClickButtonInc}/>
-            <Button name='reset' disabled={counter === MIN_VALUE} onClick={onClickButtonReset}/>
+            <Button name='inc' disabled={incDisabledValue} onClick={onClickButtonInc}/>
+            <Button name='reset' disabled={resetDisabledValue}
+                    onClick={onClickButtonReset}/>
         </div>
+
     )
+
 }
