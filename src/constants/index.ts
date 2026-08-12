278986f116c792a5ffb362a4297c8f411cbde6ce
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-08-12 15:29:05 +0800

    Refactoring add index.ts

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const STORAGE_KEY = 'counterValue'
+export const DEFAULT_SETTINGS = { max: 5, start: 0}
+
+export const ERROR_MESSAGES = {
+    INCORRECT: 'Incorrect value',
+    ENTER_VALUES: 'Enter value and press "set"',
+} as const // as const - делает объект readonly
