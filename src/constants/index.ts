2872d10febbfb9b6fd376256214b17117a59598b
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-08-12 15:48:08 +0800

    Refactoring index.tsx

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,9 @@
 export const STORAGE_KEY = 'counterValue'
-export const DEFAULT_SETTINGS = { max: 5, start: 0}
+
+export const DEFAULT_SETTINGS = {
+    max: 5,
+    start: 0
+} as const
 
 export const ERROR_MESSAGES = {
     INCORRECT: 'Incorrect value',
