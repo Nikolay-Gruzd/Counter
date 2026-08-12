307f84d74abb159c725a9c6a3bf63ae42c94b656
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-08-12 16:00:08 +0800

    Refactoring helpers.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,6 @@
-// Проверка валидности
 import {SettingType} from "../App.tsx";
 
+// Проверка валидности
 export const isSettingValid = (setting: SettingType): boolean => {
     return setting.start >= 0 && setting.max >=1 && setting.start < setting.max
 }
@@ -10,8 +10,21 @@ export const isSettingChanged = (current: SettingType, saved: SettingType): bool
     return current.start !== saved.start || current.max !== saved.max
 }
 
+// Получение значения
 export const getCounterDisplay = (counter: number, setting: SettingType, localValue?: SettingType): string | number => {
     if (!isSettingValid(setting)) return 'incorrect value'
     if (isSettingChanged(setting, localValue || setting)) return 'Enter values and press set'
     return counter
+}
+
+// Получение значения для counter className
+export const getCounterClassName = (counter: number, setting: SettingType, localValue: SettingType): string => {
+    const display = getCounterDisplay(counter, setting, localValue)
+    if (typeof display === 'string') {
+        return 'error'
+    }
+    if (counter === setting.max) {
+        return 'limitCounter'
+    }
+    return ''
 }
