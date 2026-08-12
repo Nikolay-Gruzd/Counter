73d91408202bf3ff6175925a6f2a6d9d667f215f
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-08-12 15:42:15 +0800

    Refactoring App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import s from './App.module.css'
-import {useEffect, useState} from "react";
 import {Counter} from "./components/Counter/Counter.tsx";
 import {ButtonsContainer} from "./components/ButtonsContainer/ButtonsContainer.tsx";
 import {Setting} from "./components/Setting/Setting.tsx";
-
-// export const MAX_VALUE = 5
-// export const MIN_VALUE = 0
+import {useLocalStorage} from "./hooks/useLocalStorage.ts";
+import {DEFAULT_SETTINGS, STORAGE_KEY} from "./constants";
+import {useCounter} from "./hooks/useCounter.ts";
 
 export type SettingType = {
     max: number,
@@ -13,36 +12,69 @@ export type SettingType = {
 }
 
 function App() {
-    const [localValue, setLocalValue] = useState<SettingType>({max: 5, start: 0})
 
-    const [setting, setSetting] = useState<SettingType>({max: localValue.max, start: localValue.start})
-    const [counter, setCounter] = useState<number>(localValue.start)
-    const [error, setError] = useState<string>('')
+    const { localValue, setLocalValue } = useLocalStorage(STORAGE_KEY, DEFAULT_SETTINGS)
 
-    useEffect(() => {
-        let valueAssString = localStorage.getItem('counterValue')
-        if (valueAssString) {
-            let newValue = JSON.parse(valueAssString)
-            setLocalValue(newValue)
-            setSetting(newValue)
-            setCounter(newValue.start)
-        }
-    }, [])
-    useEffect(() => {
-        localStorage.setItem('counterValue', JSON.stringify(localValue))
-    }, [localValue])
+    const {
+        counter,
+        setCounter,
+        error,
+        setError,
+        setting,
+        setSetting
+    } = useCounter(localValue)
+
+    // const [localValue, setLocalValue] = useState<SettingType>({max: 5, start: 0})
+    //
+    // const [setting, setSetting] = useState<SettingType>({max: localValue.max, start: localValue.start})
+    // const [counter, setCounter] = useState<number>(localValue.start)
+    // const [error, setError] = useState<string>('')
+    //
+    // useEffect(() => {
+    //     let valueAssString = localStorage.getItem('counterValue')
+    //     if (valueAssString) {
+    //         let newValue = JSON.parse(valueAssString)
+    //         setLocalValue(newValue)
+    //         setSetting(newValue)
+    //         setCounter(newValue.start)
+    //     }
+    // }, [])
+    // useEffect(() => {
+    //     localStorage.setItem('counterValue', JSON.stringify(localValue))
+    // }, [localValue])
 
     return (
         <div className={s.app}>
             <div className={s.container}>
-                <Setting setting={setting} localValue={localValue} setSetting={setSetting} setCounter={setCounter}
-                         setError={setError}/>
-                <ButtonsContainer modeSetting={true} setting={setting} localValue={localValue} setError={setError}
-                                  setLocalValue={setLocalValue}/>
+                <Setting
+                    setting={setting}
+                    setSetting={setSetting}
+                    setCounter={setCounter}
+                    setError={setError}
+                    localValue={localValue}
+                />
+                <ButtonsContainer
+                    modeSetting={true}
+                    setting={setting}
+                    localValue={localValue}
+                    setLocalValue={setLocalValue}
+                    setError={setError}
+                />
             </div>
             <div className={s.container}>
-                <Counter counter={counter} setting={setting} error={error} localValue={localValue}/>
-                <ButtonsContainer counter={counter} error={error} setCounter={setCounter} setting={setting}/>
+                <Counter
+                    counter={counter}
+                    setting={setting}
+                    error={error}
+                    localValue={localValue}
+                />
+                <ButtonsContainer
+                    counter={counter}
+                    setCounter={setCounter}
+                    setting={setting}
+                    error={error}
+                    setError={setError}
+                />
             </div>
         </div>
     )
